500bcf53fb868ea2aa14ebf267271da4b6e5b8ae
Sujoy Das <47408361+Sujoydas692@users.noreply.github.com>
2025-05-19 15:00:30 +0600

    project done(add on going & not done task)

--- todo.js
+++ todo.js
@@ -1,19 +1,26 @@
 let lists = [
     { title: "Do homework", done: false },
     { title: "Learn JS", done: true },
-    { title: "Learn PHP", done: false },
+    { title: "Learn PHP", done: null },
     { title: "Sleeping", done: true },
+    { title: "Eating", done: false },
+    { title: "Reading Book", done: null },
 ];
 
 // done = true
 let doneLists = [];
 
 // done = false
+let notDoneLists = [];
+
+// done = null
 let onGoingLists = [];
 
 lists.forEach((list) => {
-    if (list.done) {
+    if (list.done === true) {
         doneLists.push(list);
+    } else if (list.done === false) {
+        notDoneLists.push(list);
     } else {
         onGoingLists.push(list);
     }
@@ -26,5 +33,10 @@ doneLists.forEach((list) => {
 
 console.log("\n⏳ On Going Todos: ");
 onGoingLists.forEach((list) => {
+    console.log(`- ${list.title}`);
+});
+
+console.log("\n❌ Not Done Todos: ");
+notDoneLists.forEach((list) => {
     console.log(`- ${list.title}`);
 });
